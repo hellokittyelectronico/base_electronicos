0cfce24cd781199bc050671761c54921f5d08de1
root <root@DESKTOP-QJR8HNQ>
2024-04-09 14:39:22 +0200

    modificar url

--- models/models.py
+++ models/models.py
@@ -14,7 +14,7 @@ class base_electronicos(models.Model):
         ('Nota_credito', 'Nota_credito'),
         ('Documento_soporte', 'Documento_soporte'),
     ], string='Tipo documento')
-    url = fields.Char("URL endpoint",default="https://conector.navegasoft.com/") #url 
+    url = fields.Char("URL endpoint",default="https://conector.navegasoft.com/admonclientes") #url 
     mp_id = fields.One2many('base_electronicos.line','mp_id') #, ondelete='cascade'
 
 
